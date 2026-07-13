efae5c197eeb38a57ffb74de1b9545f834b26c00
Prasanya20 <prasanya.medapati2006@gmail.com>
2026-07-13 21:49:58 +0530

    Create ExceptionThrowerTest.java

---
+++ Exercise-4-Exception-Testing/ExceptionThrowerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ExceptionThrowerTest {
+
+    @Test
+    void testThrowException() {
+
+        ExceptionThrower exceptionThrower = new ExceptionThrower();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            exceptionThrower.throwException();
+        });
+
+    }
+}
