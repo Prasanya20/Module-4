0945e168bfc4fe1d836549b6a3bd7c957b4a527a
Prasanya20 <prasanya.medapati2006@gmail.com>
2026-07-19 19:29:11 +0530

    Create MockitoDepsApplication.java

---
+++ Mocking Dependencies in Spring Tests using Mockito/src/main/java/MockitoDepsApplication.java
@@ -0,0 +1,12 @@
+package com.example.mockitodeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoDepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoDepsApplication.class, args);
+    }
+}
